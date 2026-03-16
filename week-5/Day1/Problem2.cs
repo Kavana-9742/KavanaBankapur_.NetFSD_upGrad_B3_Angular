491275ec9fa1ffae808de23cec12b395b3ff6ebe
Kavana-9742 <kavanaforp7987@gmail.com>
2026-03-16 21:17:21 +0600

    Create Problem2.cs

---
+++ week-5/Day1/Problem2.cs
@@ -0,0 +1,44 @@
+namespace Employee
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double BaseSalary { get; set; }
+        public virtual double CalculateSalary()
+        {
+            return BaseSalary;
+        }
+    }
+    class Manager : Employee
+    {
+        public override double CalculateSalary()
+        {
+            return BaseSalary + (BaseSalary * 0.20);
+        }
+    }
+    class Developer : Employee
+    {
+        public override double CalculateSalary()
+        {
+            return BaseSalary + (BaseSalary * 0.10);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Base Salary: ");
+            double salary = Convert.ToDouble(Console.ReadLine());
+
+            Employee emp;
+
+            emp = new Manager();
+            emp.BaseSalary = salary;
+            Console.WriteLine("Manager Salary = " + emp.CalculateSalary());
+
+            emp = new Developer();
+            emp.BaseSalary = salary;
+            Console.WriteLine("Developer Salary = " + emp.CalculateSalary());
+        }
+    }
+}
