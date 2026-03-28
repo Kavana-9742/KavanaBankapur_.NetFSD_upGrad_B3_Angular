e4d1a090315e91623b543339da0c3e933987f01b
Kavana-9742 <kavanaforp7987@gmail.com>
2026-03-28 13:29:50 +0600

    Create Product.cs

---
+++ week-6/Day2/Problem1/Product.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    internal class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
