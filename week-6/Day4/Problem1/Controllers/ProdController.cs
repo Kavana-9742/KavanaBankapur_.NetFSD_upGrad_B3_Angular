c0bd1d9717fb5a2ec251516ec90880b1040ca297
Kavana-9742 <kavanaforp7987@gmail.com>
2026-03-29 11:57:52 +0600

    Add files via upload

---
+++ week-6/Day4/Problem1/Controllers/ProdController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+namespace WebApplication2.Controllers
+{
+    public class ProdController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { PId = 1, PName = "Laptop", Category = "electronics", Price = 60000},
+                new Product { PId = 2, PName = "Mobile", Category = "electronics", Price = 20000},
+                new Product { PId = 3, PName = "Keyboard", Category = "electronics", Price = 5000},
+                new Product { PId = 4, PName = "Table", Category = "Furniture", Price = 20000},
+                new Product { PId = 5, PName = "Mouse", Category = "electronics", Price = 2000}
+            };
+            return View(products);
+        }
+
+        public IActionResult Details()
+        {
+            Product prodObj = new Product()
+            {
+                PId = 101,
+                PName = "Chair",
+                Category = "Furniture",
+                Price = 35000
+            };
+            return View(prodObj);
+        }
+    }
+}
---
+++ week-6/Day4/Problem1/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int PId { get; set; }
+        public string PName { get; set; }
+        public string Category { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
