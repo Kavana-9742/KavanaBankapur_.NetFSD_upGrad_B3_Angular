041874f9267900d05bfb3cb228283dd3bb1ddb12
Kavana-9742 <kavanaforp7987@gmail.com>
2026-04-01 09:57:53 +0600

    Add files via upload

---
+++ week-7/Day1/Problem1/Controller/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication3.Controllers
+{
+    public class ContactController : Controller
+    {
+        public static List<Contact> contacts = new List<Contact>();
+        public IActionResult ShowContacts()
+        {
+            return View(contacts);
+        }
+
+        public IActionResult GetContactById(int id)
+        {
+            var contact = contacts.FirstOrDefault(c => c.ContactId == id);
+            return View(contact);
+        }
+
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                contacts.Add(contact);
+                return RedirectToAction("ShowContacts");
+            }
+            return View();
+        }
+    }
+}
---
+++ week-7/Day1/Problem1/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using WebApplication3.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "Contact Id is required")]
+        public int ContactId { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Company Name is required")]
+        public string CompanyName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email address")]
+        public string EmailId { get; set; }
+
+        [Required(ErrorMessage = "Mobile number is required")]
+        [RegularExpression(@"^[6-9]\d{9}$", ErrorMessage = "Enter valid 10-digit mobile number")]
+        public long MobileNo { get; set; }
+
+        [Required(ErrorMessage = "Designation is required")]
+        public string Designation { get; set; }
+    }
+}
