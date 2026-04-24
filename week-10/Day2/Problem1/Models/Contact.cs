d16f9acbdbc27cf1a581b41058ae254ecadbbfb2
Kavana-9742 <kavanaforp7987@gmail.com>
2026-04-24 10:43:43 +0600

    Add files via upload

---
+++ week-10/Day2/Problem1/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace week_10_day2.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ week-10/Day2/Problem1/Services/IContactService.cs
@@ -0,0 +1,13 @@
+﻿using week_10_day2.Models;
+
+namespace week_10_day2.Services
+{
+    public interface IContactService
+    {
+        List<Contact> GetAllContacts();
+        Contact? GetContactById(int id);
+        void AddContact(Contact contact);
+        void UpdateContact(Contact contact);
+        void DeleteContact(int id);
+    }
+}
